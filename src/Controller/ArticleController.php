943af18074efca4c90618f299d2d775cba3f2742
Derek Boerger <derek.boerger@mcnhealthcare.com>
2018-12-04 07:24:20 -0700

    composer updates comitted

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    public function homepage()
+    {
+        
+        return new Response('Test Page');
+    }
+    
+    public function show()
+    {
+        return new Response('Future page to show one space article.')
+    }
+}
+
+?>
