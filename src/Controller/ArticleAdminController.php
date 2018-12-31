a4f266f18924683a7f3bf1df6f408aa598a2f1a7
Derek Boerger <dracken@drackenslair.com>
2018-12-31 14:49:52 -0700

    in which our hero adds a dependant drop down menu for location using JS and API

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -155,4 +155,27 @@ EOF
             'articles' => $articles,
         ]);
     }
+
+    /**
+     * @Route("/admin/article/location-select", name="admin_article_location_select")
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function getSpecificLocationSelect(Request $request)
+    {
+        $article = new Article();
+        $article->setLocation($request->query->get('location'));
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        // no field?  Return an empty response
+        if (!$form->has('specificLocationName')) {
+            return new Response(null, 204);
+        }
+
+        return $this->render('article_admin/_specific_location_name.html.twig', [
+            'articleForm' => $form->createView(),
+            ]);
+
+    }
 }
